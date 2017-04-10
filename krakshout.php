d7c862b430b00ce32af079390cb3c507c5bd9819
Sean Hill <azusdevelopment@gmail.com>
2017-04-10 09:53:43 -0700

    Update krakshout.php

--- krakshout.php
+++ krakshout.php
@@ -19,5 +19,20 @@ $allowed_actions = array(
           'updateshout',
           'archive'
 );
+$action = (isset($_GET['action']) ? $_GET['action'] : false);
+if (!in_array($action, $allowed_actions) || !$settings['krakatoa_enabled'])
+{
+     exit;
+}
+$lang->load('krakatoa');
+$uid              = $krakatoa->userinfo['uid'];
+$ugid             = $ipb->user['usergroup'];
+$bold             = $krakatoa->userinfo['bold'];
+$admin            = $krakatoa->admin;
+$mod              = ($admin) ? true : krakatoa ->mod;
+$italic           = $krakatoa->userinfo['italic'];
+$underline        = $krakatoa->userinfo['underline'];
+$banned           = $krakatoa->banned;
+$silenced         = $krakatoa->userinfo['silenced'];
 
 ?>
